6e78c4418ed73f4afa9f6298de602a841b5dea01
Fernando Kenji Sakabe <fernandokenjisakabe@MacBook-Pro-de-Fernando.local>
2018-01-27 00:15:18 -0200

    versão MVPsem footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,10 +10,19 @@ export class AppComponent {
   lista = [];
   dados = {preco: "", comida: "", imagem: "", descricao: ""};
 
+  constructor(){
+    let dadosString = localStorage.getItem('cards');
+
+    if(dadosString){
+      this.lista = JSON.parse(dadosString);
+    }
+  }
+  
   inserirDados(){
     this.lista.push(this.dados);
     console.log(this.lista);
-    
-
+    this.dados = {preco: "", comida: "", imagem: "", descricao: ""};
+    let dadosString = JSON.stringify(this.lista);
+    localStorage.setItem('cards', dadosString);
   }
 }
